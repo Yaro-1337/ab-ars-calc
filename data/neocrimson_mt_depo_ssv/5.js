30a06ea992da207778aa772722f9c1af0d089210
Yaro-1337 <srvyaroland@gmail.com>
2026-03-22 19:36:10 +0300

    Переделал ветку по Ч2

--- data/neocrimson_mt_depo_ssv/5.js
+++ data/neocrimson_mt_depo_ssv/5.js
@@ -8,8 +8,8 @@ lines['neocrimson_mt_depo_ssv']['5'] = [
   arsAllSteps: false,
   tStay: 25,
   K: 1.5,
-  interval: 15,
-  minInterval: 0.5,
+  interval: 20,
+  minInterval: 0.1,
   slopes: {
     0: 0,
     265: -40,
@@ -25,9 +25,9 @@ lines['neocrimson_mt_depo_ssv']['5'] = [
   joints: [
     { x: 0, name: '5СП', limit: 0 },
     { x: 0 + 75, name: '6г', limit: 40, later: { 40: 2 } },
-    { x: 0 + 75 + 87.5, name: '6в', limit: 40, later: { 40: 1 } },
+    { x: 0 + 75 + 50, name: '6в', limit: 40, later: { 40: 1 } },
     { x: 0 + 75 + 87.5 + 37.5, name: '6б', limit: 40 },
-    { x: 0 + 75 + 87.5 + 37.5 + 87.5, name: '6а', limit: 40, later: { 40: 1 } },
+    { x: 0 + 75 + 87.5 + 37.5 + 50, name: '6а', limit: 40, later: { 40: 1 } },
     { x: 0 + 75 + 87.5 + 37.5 + 87.5 + 50, name: '6', limit: 40, later: { 40: 1 } },
     { x: 0 + 75 + 87.5 + 37.5 + 87.5 + 50 + 50, name: '380д', limit: 40 },
     { x: 0 + 75 + 87.5 + 37.5 + 87.5 + 50 + 50 + 50, name: '380г', limit: 40 },
@@ -46,8 +46,9 @@ lines['neocrimson_mt_depo_ssv']['5'] = [
 
     // { joint: '369а', name: '371', lenses: '', guard: 80, shift: 0 },
     { joint: '380г', name: 'МТ-376м', lenses: 'yYY-GRw-M', autostop: 1, shift: 0, back: true, left: true }, 
-    { joint: '6б', name: 'Ч2', lenses: 'YYG-Rw', autostop: 1, shift: 0, back: true, left: true }, 
-    { joint: '6г', name: 'М6', lenses: 'WR-w', shift: 0, back: true, left: true },
+    { joint: '6в', name: 'Ч2', lenses: 'YG-Rw', autostop: 1, shift: 0, back: true, left: true }, 
+    { joint: '6а', name: 'Ч4', lenses: 'YYG-Rw', autostop: 1, shift: 0, back: true, left: true }, 
+    { joint: '6г', name: 'М6', lenses: 'WR-w', shift: 0, back: true, left: true, row: -1 },
   ],
   mks: [
   ],
